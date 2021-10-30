8a7bd50b2e20f968757d27e9f6cc51d3bd88c784
Arpit Mittal <youreka468@gmail.com>
2021-10-30 20:55:58 +0530

    Commit

--- backend/login.js
+++ backend/login.js
@@ -12,16 +12,30 @@ const { sanitizePhone } = require('./sanitize');
 function loginAPI(req, res) {
 
     // sanitize params 
-    let phoneNum = getParams(req.url, ['phonenumber'])
-    console.log(phoneNum)
+    let phoneNum = getParams(req.url, ['phonenumber'])['phonenumber']
 
+    let STATUS = '', MESSAGE = '';
+    
     // logic
+    if (phoneNum == null) {
+        STATUS = 'failed'
+        MESSAGE = 'Bad Request'
+    }
+    else if (!isValidPhone(phoneNum)) {
+        STATUS = 'failed'
+        MESSAGE = 'Invalid Phone Number'
+    }
+    else {
+        STATUS = 'ok'
+        MESSAGE = 'Login Attemp Succesfull/ Otp Sent to Mobile'
+        // send otp 
+    }
 
-    let resulJSON = '{"STATUS":"ok", "MESSAGE": "working/login"}'
+    let resulJSON = `{"STATUS" : "${STATUS}", "MESSAGE": "${MESSAGE}"}`
 
     res.writeHead(200, { "content-type": "application/json" });
     res.end(resulJSON);
-    
+
 }
 
 module.exports = { loginAPI }
