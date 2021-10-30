5103067a08274746463b089cb09684fac01bc5d6
Arpit Mittal <youreka468@gmail.com>
2021-10-30 18:44:13 +0530

    Commit

---
+++ backend/getParams.js
@@ -0,0 +1,29 @@
+
+/**
+ * To Extract params from a URL 
+ * url = string
+ * paramList = list of string 
+ * return object , value may be null if not found
+*/
+
+function getParams(url, paramList) {
+
+    let result = {};
+    for (let i of paramList) result[i] = null
+
+    url = url.split('?')
+    if (url.lenght == 1)
+        return result
+    else url = url[1]
+
+    let searcher = new URLSearchParams(url)
+
+    for (let p of paramList) {
+        if (searcher.has(p))
+            result[p] = searcher.get(p)
+    }
+
+    return result
+}
+
+module.exports = { getParams }
---
+++ backend/sanitize.js
@@ -0,0 +1,4 @@
+function sanitizePhone(pn){
+
+}
+
