981299f38a22853872a3770a439585cd61ebdb28
Arpit Mittal <youreka468@gmail.com>
2021-10-30 19:08:27 +0530

    Sanitize

--- backend/sanitize.js
+++ backend/sanitize.js
@@ -1,4 +1,11 @@
-function sanitizePhone(pn){
 
+/**
+ * pn = string or number
+ * return string (phone Number ) if valid
+ * else null
+*/
+function sanitizePhone(pn) {
+    pn = String(pn)
+    const phonereg = /^\d{10}$/;
+    return phonereg.test(pn) ? ph : null;
 }
-
